09960b276c07582e1b835ce0d800e73e6fb326e2
greetingsfellowhumans <accounts@aaronjprice.com>
2026-09-19 16:18:00 -0700

    feat: add Transform.Move module

---
+++ test/curves/transform/move_test.exs
@@ -0,0 +1,48 @@
+defmodule Curves.Transform.MoveTest do
+  use ExUnit.Case
+  alias Curves.Transform.Move
+  doctest Move
+  import Curves.Support.SampleCurves
+
+  setup [:with_curves]
+
+
+  test "should move a bezier spline", ctx do
+    curve = ctx.curves.curve1
+    {x, y} = Curves.solve!(curve, 0.0)
+    assert x == 5.0
+    assert y == 10.0
+
+    {x, y} = Curves.solve!(curve, 0.25)
+    assert x == 7.1875
+    assert y == 9.21875
+
+    curve = Move.inc_x(curve, 10)
+    {x, y} = Curves.solve!(curve, 0.0)
+    assert x == 15.0
+    assert y == 10.0
+
+    {x, y} = Curves.solve!(curve, 0.25)
+    assert x == 17.1875
+    assert y == 9.21875
+
+    curve = Move.inc_y(curve, 10)
+    {x, y} = Curves.solve!(curve, 0.0)
+    assert x == 15.0
+    assert y == 20.0
+
+    {x, y} = Curves.solve!(curve, 0.25)
+    assert x == 17.1875
+    assert y == 19.21875
+
+    curve = Move.inc_position(curve, {-10, -10})
+    {x, y} = Curves.solve!(curve, 0.0)
+    assert x == 5.0
+    assert y == 10.0
+
+    curve = Move.set_position(curve, {-10, -10})
+    {x, y} = Curves.solve!(curve, 0.0)
+    assert x == -5.0
+    assert y == 0.0
+  end
+end
